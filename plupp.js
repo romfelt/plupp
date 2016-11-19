642fce66111fb32031f15aaefe0dfc2e7cebd71b
Romfelt, Jonas <Jonas.Romfelt@flir.se>
2016-11-19 01:08:41 +0100

    Table building is a class

--- plupp.js
+++ plupp.js
@@ -1,118 +1,3 @@
-// set or get cell value
-function cell(tableId, column, row, value) {
-	if (typeof(value) === 'undefined') {
-		return $('#' + tableId + ' tr:eq(' + row + ') td:eq(' + column + ')').text();
-	}
-	else {
-		$('#' + tableId + ' tr:eq(' + row + ') td:eq(' + column + ')').text(value);
-	}
-}
-
-// get Float value of cell
-function cellF(tableId, column, row) {
-	var v = parseFloat(cell(tableId, column, row));
-	//console.log(column + ':' + row + ' = ' + v);
-	return !isNaN(v) ? v : 0;
-}
-
-// get sum of cell block
-function sum(tableId, column, row, width, height) {
-	var sum = 0;
-	for (var c = 0; c < width; c++) {
-		for (var r = 0; r < height; r++) {
-			sum += cellF(tableId, column + c, row + r);
-		}
-	}
-	return sum;
-}
-
-// get delta between two cells: A - B
-function delta(tableId, columnA, rowA, columnB, rowB) {
-	return cellF(tableId, columnA, rowA) - cellF(tableId, columnB, rowB);
-}
-
-function updateSums(tableId) {
-	$('#' + tableId + ' .cell-sum').each(function(i, e) {
-		var c = $(e).index();
-	    var r = $(e).closest('tr').index();
-		var s = sum(tableId, c, 1, 1, r - 1);
-
-		$(e).text(s);
-		//console.log('sum(' + c + ':' + r + ') = ' + s);
-	});
-}
-
-function updateDeltas(tableId) {
-	$('#' + tableId + ' .cell-delta').each(function(i, e) {
-		var c = $(e).index();
-	    var r = $(e).closest('tr').index();
-		var s = delta(tableId, c, r - 1, c, r - 2);
-
-		$(e).text(s);
-		//console.log('delta(' + c + ':' + r + ') = ' + s);
-	});
-}
-
-function updateTable(tableId) {
-	updateSums(tableId);
-	updateDeltas(tableId);
-}
-
-function addCells(container, data, classes) {
-	$.each(data, function(i, v) {
-		container.append($('<td/>').addClass(classes).text(v));
-	});
-}
-
-function makeTable(container, tableId, tableData, requestType, requestId) {
-	var table = $('<table id="' + tableId + '"/>').addClass('edit-table');
-
-	// store data in table element for generic ajax request to use
-	table.data('requestType', requestType);
-	table.data('requestId', requestId);
-
-	$.each(tableData, function(i, obj) {
-		var tr = $("<tr/>");
-
-		tr.append($("<td class='cell-header'/>").text(obj.title));
-		if (obj.type == 'time') {
-			addCells(tr, obj.data, 'cell-header');
-		}
-		else if (obj.type == 'editable') {
-			tr.data('id', obj.id); // id used for interfacing with database
-			addCells(tr, obj.data, 'cell');			
-		}
-		else if (obj.type == 'quota') {
-			addCells(tr, obj.data, 'cell-header');
-		}
-		else if (obj.type == 'sum') {
-			addCells(tr, new Array(obj.columns), 'cell-sum');
-		}
-		else if (obj.type == 'delta') {
-			addCells(tr, new Array(obj.columns), 'cell-delta');
-		}
-
-		table.append(tr);
-	});
-
-	container.append(table);
-	table.editableTableWidget();
-	updateTable(tableId);
-
-	$('td.cell').on('validate', function(e, newValue) {
-		// @TODO make proper input value check
-		if (newValue == 1) { 
-			return false; // mark cell as invalid 
-		}
-	});
-	
-	$('td.cell').on('change', function(e, newValue) {
-		// @TODO only update corresponding column
-		$(e.target).addClass('cell-dirty');
-		updateTable(tableId);
-		return true;
-	});
-}
 
 function doPost(tableId) {
 	// @TODO check valid data
@@ -193,12 +78,13 @@ Plupp = {
 
 
 
-function Table(periodType, startPeriod, length) {
+function Table(tableId, periodType, startPeriod, length) {
 	var self = this; // keep reference to this object to be used independent of call context
 	this.startPeriod = startPeriod;
 	this.length = length;
 	this.table = null;
 	this.zeroes = null;
+	this.tableId = tableId;
 
 	// returns array of length with pre-defined values
 	this.getArray = function(length, startValue, increment) {
@@ -251,48 +137,117 @@ function Table(periodType, startPeriod, length) {
 		self.table.push({'type': 'delta', 'title': "Delta", 'columns': length});
 	}
 
-	this.build = function() {
-		makeTable($('#table-container'), 'plan', self.table, 'plan', 66);
-	}
-}
-
-
-// make basic editable table
-function basicTable(startPeriod, length, titles, values) {
-	var zeroes = getArray(length, 0, 0);
-	var months = getArray(length, startPeriod, 1);
-	var lookup = {}; // lookup table 
-
-	// create table with headings
-	var table = [{'type': 'time', 'title': 'Month', 'data': months}];
-
-	// create default values of zero to all table cells
-	$.each(titles.data, function(i, v) {
-		var data = zeroes.slice(); // make copy of array to create new object
-		var obj = {'type': 'editable', 'id': v.id, 'title': v.name, 'data': data};
-		table.push(obj);
-		lookup[v.id] = obj; // store reference to object, instead of searching for objects
-	});
-
-	// add real cell values using lookup
-	$.each(values.data, function(i, v) {
-		if (typeof lookup[v.id] !== 'undefined') {
-			lookup[v.id].data[v.period - startPeriod] = v.value;
+	// set or get cell value
+	this.cell = function(column, row, value) {
+		if (typeof(value) === 'undefined') {
+			return $('#' + self.tableId + ' tr:eq(' + row + ') td:eq(' + column + ')').text();
 		}
-	});
+		else {
+			$('#' + self.tableId + ' tr:eq(' + row + ') td:eq(' + column + ')').text(value);
+		}
+	}
 
-	// add a sum row at the bottom
-	table.push({'type': 'sum', 'title': "Sum", 'columns': length});
+	// get Float value of cell
+	this.cellF = function(column, row) {
+		var v = parseFloat(self.cell(column, row));
+		return !isNaN(v) ? v : 0;
+	}
 
-	return table;
-}
+	// get sum of cell block
+	this.sum = function(column, row, width, height) {
+		var sum = 0;
+		for (var c = 0; c < width; c++) {
+			for (var r = 0; r < height; r++) {
+				sum += self.cellF(column + c, row + r);
+			}
+		}
+		return sum;
+	}
 
-function addDataRow(table, length, values, type, title) {
-	var data = getArray(length, 0, 0);
-	$.each(values.data, function(i, v) {
-		data[v.period - startPeriod] = v.value;
-	});
-	table.push({'type': type, 'title': title, 'data': data});  
+	// get delta between two cells: A - B
+	this.delta = function(columnA, rowA, columnB, rowB) {
+		return self.cellF(columnA, rowA) - self.cellF(columnB, rowB);
+	}
+
+	this.updateSums = function() {
+		$('#' + self.tableId + ' .cell-sum').each(function(i, e) {
+			var c = $(e).index();
+		    var r = $(e).closest('tr').index();
+			var s = self.sum(c, 1, 1, r - 1);
+			$(e).text(s);
+		});
+	}
+
+	this.updateDeltas = function() {
+		$('#' + self.tableId + ' .cell-delta').each(function(i, e) {
+			var c = $(e).index();
+		    var r = $(e).closest('tr').index();
+			var s = self.delta(c, r - 1, c, r - 2);
+			$(e).text(s);
+		});
+	}
+
+	this.updateTable = function() {
+		self.updateSums();
+		self.updateDeltas();
+	}
+
+	this.addCells = function(container, data, classes) {
+		$.each(data, function(i, v) {
+			container.append($('<td/>').addClass(classes).text(v));
+		});
+	}
+
+	this.build = function(container, requestType, requestId) {
+		var table = $('<table id="' + self.tableId + '"/>').addClass('edit-table');
+
+		// @TODO store in object instead!!
+		// store data in table element for generic ajax request to use
+		table.data('requestType', requestType);
+		table.data('requestId', requestId);
+
+		$.each(self.table, function(i, obj) {
+			var tr = $("<tr/>");
+
+			tr.append($("<td class='cell-header'/>").text(obj.title));
+			if (obj.type == 'time') {
+				self.addCells(tr, obj.data, 'cell-header');
+			}
+			else if (obj.type == 'editable') {
+				tr.data('id', obj.id); // id used for interfacing with database
+				self.addCells(tr, obj.data, 'cell');			
+			}
+			else if (obj.type == 'quota') {
+				self.addCells(tr, obj.data, 'cell-header');
+			}
+			else if (obj.type == 'sum') {
+				self.addCells(tr, new Array(obj.columns), 'cell-sum');
+			}
+			else if (obj.type == 'delta') {
+				self.addCells(tr, new Array(obj.columns), 'cell-delta');
+			}
+
+			table.append(tr);
+		});
+
+		container.append(table);
+		table.editableTableWidget();
+		self.updateTable();
+
+		$('td.cell').on('validate', function(e, newValue) {
+			// @TODO make proper input value check
+			if (newValue == 1) { 
+				return false; // mark cell as invalid 
+			}
+		});
+		
+		$('td.cell').on('change', function(e, newValue) {
+			// @TODO only update corresponding column
+			$(e.target).addClass('cell-dirty');
+			self.updateTable();
+			return true;
+		});
+	}
 }
 
 function quotasTable(startPeriod, length) {
@@ -304,14 +259,14 @@ function quotasTable(startPeriod, length) {
 		quotas.run(), projects.run()
 	)
 	.then(function() {
-		var t = new Table('month', startPeriod, length);
+		var t = new Table('quotas', 'month', startPeriod, length);
 		t.addEditDataSection(projects.reply.data, quotas.reply.data);
 		t.addSum();
 		t.addDataRow([], 'quota', 'Available');
 		t.addDelta();
 		t.addDataRow([], 'quota', 'Requested');
 		t.addDelta();
-		t.build();
+		t.build($('#table-container'), 'plan', 66);
 	})
 	.fail(function() {
 		console.log( "something went wrong!" );
@@ -327,12 +282,12 @@ function projectTable(projectId, startPeriod, length) {
 		teams.run(), quota.run(), plan.run()
 	)
 	.then(function() {
-		var t = new Table('month', startPeriod, length);
+		var t = new Table('project', 'month', startPeriod, length);
 		t.addEditDataSection(teams.reply.data, plan.reply.data);
 		t.addSum();
 		t.addDataRow(quota.reply.data, 'quota', 'Quota');
 		t.addDelta();
-		t.build();
+		t.build($('#table-container'), 'quota', 0);
 	})
 	.fail(function() {
 		console.log( "something went wrong!" );
@@ -348,9 +303,8 @@ function showView(view) {
 $(document).ready(function() {
 	console.log("ready!");
 
-//	projectTable(66, 11, 24);
-
-	quotasTable(11, 24);
+	projectTable(66, 11, 24);
+//	quotasTable(11, 24);
 
 	return;
 
