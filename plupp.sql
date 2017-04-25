cb2ff67713ccd064e848b39e10c5ec6d96f5b156
Romfelt, Jonas <Jonas.Romfelt@flir.se>
2017-04-25 08:52:19 +0200

    latest database structure

--- plupp.sql
+++ plupp.sql
@@ -3,12 +3,18 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 28, 2016 at 07:24 PM
--- Server version: 5.5.53-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.20
+-- Generation Time: Apr 25, 2017 at 08:49 AM
+-- Server version: 5.5.54-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `plupp`
@@ -17,13 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `availability`
+-- Table structure for table `allocation`
 --
 
-CREATE TABLE IF NOT EXISTS `availability` (
+CREATE TABLE IF NOT EXISTS `allocation` (
   `resourceId` int(10) unsigned NOT NULL,
-  `period` int(10) unsigned NOT NULL,
-  `value` int(10) unsigned NOT NULL,
+  `projectId` int(10) unsigned NOT NULL,
+  `period` date NOT NULL,
+  `value` decimal(3,2) unsigned NOT NULL,
+  `userId` int(10) unsigned NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `available`
+--
+
+CREATE TABLE IF NOT EXISTS `available` (
+  `resourceId` int(10) unsigned NOT NULL,
+  `period` date NOT NULL,
+  `value` decimal(3,2) unsigned NOT NULL,
+  `userId` int(10) unsigned NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -37,7 +59,21 @@ CREATE TABLE IF NOT EXISTS `department` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `label`
+--
+
+CREATE TABLE IF NOT EXISTS `label` (
+  `label` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `view` enum('plan','quota','allocation','available') COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id` int(10) unsigned DEFAULT NULL,
+  `userId` int(10) unsigned NOT NULL COMMENT 'Id of user who made the change',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -46,10 +82,11 @@ CREATE TABLE IF NOT EXISTS `department` (
 --
 
 CREATE TABLE IF NOT EXISTS `plan` (
+  `resourceId` int(10) unsigned NOT NULL,
   `projectId` int(10) unsigned NOT NULL,
-  `teamId` int(10) unsigned NOT NULL,
-  `period` int(10) unsigned NOT NULL,
-  `value` int(10) unsigned NOT NULL,
+  `period` date NOT NULL,
+  `value` decimal(3,2) unsigned NOT NULL,
+  `userId` int(10) unsigned NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -63,8 +100,9 @@ CREATE TABLE IF NOT EXISTS `project` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `priority` int(11) NOT NULL,
+  `departmentId` int(10) unsigned NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 -- --------------------------------------------------------
 
@@ -74,8 +112,9 @@ CREATE TABLE IF NOT EXISTS `project` (
 
 CREATE TABLE IF NOT EXISTS `quota` (
   `projectId` int(10) unsigned NOT NULL,
-  `period` int(10) unsigned NOT NULL,
-  `value` int(10) unsigned NOT NULL,
+  `period` date NOT NULL,
+  `value` decimal(5,2) unsigned NOT NULL,
+  `userId` int(10) unsigned NOT NULL COMMENT 'Id of user who made the change',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -90,7 +129,7 @@ CREATE TABLE IF NOT EXISTS `resource` (
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=5 ;
 
 -- --------------------------------------------------------
 
@@ -102,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `resource_data` (
   `resourceId` int(10) unsigned NOT NULL,
   `teamId` int(10) unsigned NOT NULL,
   `departmentId` int(10) unsigned NOT NULL,
-  `type` enum('employee','contractor','mobica') COLLATE utf8_unicode_ci NOT NULL,
+  `type` enum('employee','contractor','offshore') COLLATE utf8_unicode_ci NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Separate resource data that may be changed';
 
@@ -129,7 +168,7 @@ CREATE TABLE IF NOT EXISTS `team` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
 -- --------------------------------------------------------
 
@@ -142,5 +181,10 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(32) CHARACTER SET ascii NOT NULL,
   `password` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'md5 hash',
   `source` enum('database','ldap') COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'Source for verifying user acount',
+  `access` int(11) NOT NULL DEFAULT '0' COMMENT 'User access level where 0 is lowest possible',
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
